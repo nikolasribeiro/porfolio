71e0a85b625dfb28e26c17deb4cea380fb4d031d
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-11-21 17:09:42 -0300

    destroy decomposer

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p@htmwq-frkky6f*$j=z_xy*1=s^+^k7rkn6o_@+-%h#$bl^ux'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
